b97879ec46aedf5f0b9d972dd9d9bb71ef15a2f9
avle <avle@users.noreply.github.com>
2015-04-23 13:07:42 -0700

    more code

--- go.py
+++ go.py
@@ -1,4 +1,12 @@
 #first test, hello world
 
-print("Hello, World!)
+print("Hello, World!")
 
+person = input('Enter your name: ')
+print('Hello', person)
+
+'''Error in addition from input.'''
+
+x = input("Enter a number: ")
+y = input("Enter a second number: ")
+print('The sum of ', x, ' and ', y, ' is ', int(x)+int(y), '.', sep='') #error
